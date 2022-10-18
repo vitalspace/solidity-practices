794e4fe355746b29ff0b7fe477ab815a8403534c
vitalspace <myglobalsounds@gmail.com>
2022-10-17 19:41:28 -0500

    Hello world

---
+++ helloWolrd.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+// Create hello world Contract
+contract helloWorld {
+    // Set Message
+    string message = "hello Wolrd";
+
+    // Get Message
+    function getHelloWorld() public view returns (string memory) {
+        return message;
+    }
+}
